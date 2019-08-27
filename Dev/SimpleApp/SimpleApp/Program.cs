e281fe7e8eda0432e68df39f35fb65e14db7925a
Silviu Avram <DIRDUB01\irca659>
2019-08-27 13:03:21 +0100

    Dev message

--- Dev/SimpleApp/SimpleApp/Program.cs
+++ Dev/SimpleApp/SimpleApp/Program.cs
@@ -6,7 +6,8 @@ namespace SimpleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Dev!");
         }
     }
 }
+
