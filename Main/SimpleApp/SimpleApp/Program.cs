2ce69ede16d7b95a3d1f168e9022a47b681ea2d5
Silviu Avram <DIRDUB01\irca659>
2019-08-27 13:12:31 +0100

    PROD message

--- Main/SimpleApp/SimpleApp/Program.cs
+++ Main/SimpleApp/SimpleApp/Program.cs
@@ -6,7 +6,7 @@ namespace SimpleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello PROD!");
         }
     }
 }
